d9ab5d1d4d2a0bbc16fc8f9a7a355b5336413de4
Daniel Grychtol <grychtol.daniel@gmail.com>
2019-07-30 20:44:34 +0200

    Add mouse move events compatibility

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const carouselContainer = document.getElementById('carousel-container');
 const tileBox = document.querySelector('.carousel-box');
 const tiles = document.querySelectorAll('.carousel-tile');
-const nextBtn = document.querySelector('#next');
-const prevBtn = document.querySelector('#prev');
+const nextBtn = document.querySelector('#carousel-next');
+const prevBtn = document.querySelector('#carousel-prev');
 
 let offsetX = 0;
 let startPos = 0;
@@ -61,15 +61,18 @@ function nextSlide() {
 }
 
 carouselContainer.addEventListener('touchstart', function(e) {
+  e.preventDefault();
   startPos = e.changedTouches[0].clientX;
   carouselContainer.style.transition = initCarouselTransitionValue;
 });
 carouselContainer.addEventListener('touchmove', function(e) {
+  e.preventDefault();
   offsetX = e.touches[0].clientX;
   moveOffset = offsetX - startPos;
   carouselContainer.style.transform = `translateX(${moveOffset * 0.4}px)`;
 });
 carouselContainer.addEventListener('touchend', function(e) {
+  e.preventDefault();
   if (Math.abs(moveOffset) > 100) {
     if (moveOffset < 0) {
       nextSlide();
@@ -83,3 +86,86 @@ carouselContainer.addEventListener('touchend', function(e) {
 
 nextBtn.addEventListener('click', nextSlide);
 prevBtn.addEventListener('click', prevSlide);
+
+// mouse
+
+let mouseDown = false;
+let mousePath = 0;
+let startingPosX = 0;
+let trigger = false;
+let startTime = null;
+
+function resetMouseValues() {
+  mouseDown = false;
+  mousePath = 0;
+  trigger = false;
+}
+
+carouselContainer.addEventListener('mousedown', function(e) {
+  mouseDown = true;
+  mousePath = 0;
+  startingPosX = e.offsetX;
+  carouselContainer.style.transition = initCarouselTransitionValue;
+
+  startTime = new Date().valueOf();
+});
+
+carouselContainer.addEventListener('mouseleave', function(e) {
+  e.preventDefault();
+  if (Math.abs(mousePath) > 100 && trigger) {
+    if (mousePath > 0) {
+      nextSlide();
+    } else {
+      prevSlide();
+    }
+  } else {
+    carouselContainer.style.transform = `translateX(0)`;
+  }
+
+  resetMouseValues();
+});
+
+carouselContainer.addEventListener('mouseup', function(e) {
+  //   copied from touch
+  e.preventDefault();
+  if (Math.abs(mousePath) > 100 && trigger) {
+    if (mousePath > 0) {
+      nextSlide();
+    } else {
+      prevSlide();
+    }
+  } else {
+    carouselContainer.style.transform = `translateX(0)`;
+  }
+
+  resetMouseValues();
+});
+
+carouselContainer.addEventListener('mousemove', function(e) {
+  e.preventDefault();
+  mousePath = startingPosX - e.offsetX;
+
+  let czas = (new Date().valueOf() - startTime) / 1000;
+
+  let predkosc = Math.abs(mousePath) / czas;
+
+  if (predkosc > 450) {
+    trigger = true;
+  }
+
+  if (mouseDown && trigger) {
+    carouselContainer.style.transform = `translateX(${-mousePath * 0.4}px)`;
+  }
+});
+
+// SCROLL WHEEL / touchpad
+
+// carouselContainer.onwheel = function(e) {
+//   e.preventDefault();
+//   console.log(e.deltaX);
+//   carouselContainer.style.transform = `translateX(${e.deltaX * 2.5}px)`;
+//   if (Math.abs(e.deltaX) > 50) {
+//     nextSlide();
+//     carouselContainer.style.transform = `translateX(0)`;
+//   }
+// };
